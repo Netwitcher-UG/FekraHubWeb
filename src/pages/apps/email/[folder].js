ca687a8691f9fc11c28d15ace5c30973bfb92acc
Remoan-Toumani <remoantoumani@icloud.com>
2025-11-04 01:30:18 +0300

    refactor: Replace getStaticProps with getServerSideProps in email folder and label pages to enable dynamic data fetching at request time

--- src/pages/apps/email/[folder].js
+++ src/pages/apps/email/[folder].js
@@ -1,6 +1,3 @@
-// ** Third Party Imports
-import axios from 'axios'
-
 // ** Demo Components Imports
 import Email from 'src/views/apps/email/Email'
 
@@ -8,24 +5,12 @@ const EmailApp = ({ folder }) => {
   return <Email folder={folder} />
 }
 
-export const getStaticPaths = async () => {
-  const res = await axios.get('/apps/email/allEmails')
-  const data = await res.data.emails
-
-  const paths = data.map(mail => ({
-    params: { folder: mail.folder }
-  }))
-
-  return {
-    paths,
-    fallback: false
-  }
-}
-
-export const getStaticProps = ({ params }) => {
+// Use getServerSideProps to avoid build-time API calls
+// This runs at request time, not build time
+export const getServerSideProps = async ({ params }) => {
   return {
     props: {
-      folder: params?.folder
+      folder: params?.folder || 'inbox'
     }
   }
 }
