60562325565952c3419d1c5989c05e965056e05d
Remoan-Toumani <remoantoumani@icloud.com>
2025-12-14 11:53:43 +0300

    feat: Implement fetchEmployeesPayrollInfo action to retrieve payroll data for employees and update PayrollDataGrid component accordingly

--- src/pages/payroll/index.js
+++ src/pages/payroll/index.js
@@ -1,24 +1,22 @@
 import React, { useEffect } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
-import { fetchEmployees } from 'src/store/apps/users'
 import PayrollDataGrid from 'src/views/payroll/dataGrid'
 import Grid from '@mui/material/Grid'
 import Card from '@mui/material/Card'
+import { fetchEmployeesPayrollInfo } from 'src/store/apps/payroll'
 
 export default function Index() {
-  const { employeesData, loading } = useSelector(state => state.users)
-
+  const { employeesPayrollInfo, employeesPayrollInfoLoading } = useSelector(state => state.payroll)
   const dispatch = useDispatch()
 
   useEffect(() => {
-    dispatch(fetchEmployees('RoleName=Teacher&RoleName=Secretariat'))
+    dispatch(fetchEmployeesPayrollInfo())
   }, [dispatch])
-
   return (
     <Grid container spacing={6.5} sx={{ height: 'calc(100vh - 145px)', overflow: 'hidden' }}>
       <Grid item xs={12} sx={{ height: '100%', display: 'flex', flexDirection: 'column' }}>
         <Card sx={{ height: '100%', display: 'flex', flexDirection: 'column', overflow: 'hidden' }}>
-          <PayrollDataGrid rows={employeesData} loading={loading} />
+          <PayrollDataGrid rows={employeesPayrollInfo} loading={employeesPayrollInfoLoading} />
         </Card>
       </Grid>
     </Grid>
